5b414f32a57f82646305ca0b7c0dc2bfce2ce7ab
Kharente Deuh <33319475+Kharente-Deuh@users.noreply.github.com>
2026-08-14 01:27:37 +0200

    feat(comics): add status and type chips

---
+++ web/app/features/comics/components/Chip/Type.vue
@@ -0,0 +1,15 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<script setup lang="ts">
+import type { ComicType } from '../../types'
+
+defineProps<{ type: ComicType }>()
+</script>
+
+<template>
+  <span
+    class="ga-3 w-fit px-2 py-1 border-thin text-body-large text-truncate text-uppercase bg-background"
+    :style="{ borderRadius: '12px' }"
+  >
+    <span class="">{{ $t(`sources.asura.type.${type}`) }}</span>
+  </span>
+</template>
