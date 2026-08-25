0267a487bf7f3d2d058d0b8e7c6ca15ec62d1e81
Kharente Deuh <33319475+Kharente-Deuh@users.noreply.github.com>
2026-08-25 17:23:36 +0200

    feat: KingOfShojo source app with shared series cache

--- api/pkg/sources/kingofshojo/pkg/domain/apiclient.go
+++ api/pkg/sources/kingofshojo/pkg/domain/apiclient.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/kharente-deuh/uchiyomi-server/pkg/sources"
 	"github.com/kharente-deuh/uchiyomi-server/pkg/sources/kingofshojo/pkg/parse"
+	"github.com/kharente-deuh/uchiyomi-server/pkg/utils"
 	"github.com/kharente-deuh/uchiyomi-server/pkg/utils/challengesolver"
 )
 
@@ -48,6 +50,44 @@ type SearchCacheOpts struct {
 	Limit     int
 }
 
+type SearchOpts struct {
+	Search    string
+	Sort      SortType
+	SortOrder SortOrder
+	Offset    int
+	Limit     int
+	UserID    uuid.UUID
+}
+
+type SearchResult struct {
+	Items []SearchResultItem
+	Meta  SearchResultMeta
+}
+
+type SearchResultItem struct {
+	LastChapterAt  *time.Time
+	UpdatedAt      time.Time
+	CreatedAt      time.Time
+	InternalID     *uuid.UUID
+	Description    string
+	PublicURL      string
+	Status         sources.SeriesStatus
+	Type           sources.SeriesType
+	Author         string
+	Artist         string
+	SourceURL      string
+	Slug           string
+	Title          string
+	Cover          string
+	AltTitles      []string
+	LatestChapters []SearchResultItemChapter
+	Genres         []string
+	ChapterCount   int
+	ID             int
+	Rating         float64
+	ReleaseYear    int
+}
+
 type SearchCacheResult struct {
 	Items []SearchCacheResultItem
 	Meta  SearchResultMeta
@@ -82,12 +122,49 @@ type SearchCacheResultItem struct {
 	ReleaseYear    int
 }
 
+func (i *SearchCacheResultItem) Domain(internalID *uuid.UUID) SearchResultItem {
+	return SearchResultItem{
+		LastChapterAt:  utils.OptionalTime(i.LastChapterAt),
+		UpdatedAt:      i.UpdatedAt,
+		CreatedAt:      i.CreatedAt,
+		PublicURL:      i.PublicURL,
+		SourceURL:      i.SourceURL,
+		Cover:          i.Cover,
+		Status:         i.Status,
+		Type:           i.Type,
+		Author:         i.Author,
+		Artist:         i.Artist,
+		Description:    i.Description,
+		Slug:           i.Slug,
+		Title:          i.Title,
+		AltTitles:      i.AltTitles,
+		Genres:         i.Genres,
+		LatestChapters: i.LatestChapters,
+		ChapterCount:   i.ChapterCount,
+		ID:             i.ID,
+		Rating:         i.Rating,
+		ReleaseYear:    i.ReleaseYear,
+		InternalID:     internalID,
+	}
+}
+
 type SearchResultItemChapter struct {
 	EarlyAccessUntil *time.Time
 	PublishedAt      time.Time
 	Title            string
 	ID               string
 	Number           float64
+}
+
+type Chapter struct {
+	EarlyAccessUntil *time.Time
+	PublishedAt      time.Time
+	InternalID       *uuid.UUID
+	Download         *int
+	ID               string
+	Title            string
+	Number           float64
+	PageCount        int
 }
 
 type GetImageURLsByChapterOpts struct {
